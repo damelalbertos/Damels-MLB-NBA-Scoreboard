f8de57496065213248d20f0709d9212cf8c9808e
Damel Albertos <damelalbertos@icloud.com>
2024-12-22 18:37:29 -0500

    fix ui bug

--- src/process.js
+++ src/process.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 import * as constants from "./constants";
-const CONFIG = require('./config.js');
-const mlbData = require('./mlb_test.json');
-const nbaData = require('./nba_test.json');
+
+const CONFIG = require("./config.js");
+const mlbData = require("./mlb_test.json");
+const nbaData = require("./nba_test.json");
 
 /**
  * This function preps and makes a GET call to the MLB Stats API. This is needed to gather the data that will be
@@ -13,22 +14,22 @@ const nbaData = require('./nba_test.json');
  * @returns {Object} Get Response data is returned
  */
 async function getGameData(sport, params, endpoint) {
-    const options = {
-        method: 'GET',
-        url: `https://${CONFIG.hostname[sport]}/${endpoint}`,
-        params,
-        headers: {
-            'X-RapidAPI-Key': CONFIG.apiKey,
-            'X-RapidAPI-Host': CONFIG.hostname[sport]
-        }
-    };
-
-    try {
-        const response = await axios.request(options);
-        return response.data;
-    } catch (error) {
-        await console.error(error);
+  const options = {
+    method: "GET",
+    url: `https://${CONFIG.hostname[sport]}/${endpoint}`,
+    params,
+    headers: {
+      "X-RapidAPI-Key": CONFIG.apiKey,
+      "X-RapidAPI-Host": CONFIG.hostname[sport]
     }
+  };
+
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    await console.error(error);
+  }
 }
 
 /**
@@ -39,18 +40,20 @@ async function getGameData(sport, params, endpoint) {
  * @returns {String} Projected winning team based on sports betting data OR No Projection if data is not available
  */
 function calculateProjection(gameOdds, game) {
-    if (!gameOdds.error && hasBettingOdds(gameOdds)) {
-        const { bet365: { awayTeamRunLine, homeTeamRunLine } } = gameOdds.body[Object.keys(gameOdds.body)[0]];
-        const { away, home }  = game;
+  if (!gameOdds.error && hasBettingOdds(gameOdds)) {
+    const {
+      bet365: { awayTeamRunLine, homeTeamRunLine }
+    } = gameOdds.body[Object.keys(gameOdds.body)[0]];
+    const { away, home } = game;
 
-        if (awayTeamRunLine < homeTeamRunLine) {
-            return away;
-        } else if (homeTeamRunLine > awayTeamRunLine) {
-            return home;
-        }
+    if (awayTeamRunLine < homeTeamRunLine) {
+      return away;
+    } else if (homeTeamRunLine > awayTeamRunLine) {
+      return home;
     }
+  }
 
-    return 'No Projection';
+  return "No Projection";
 }
 
 /**
@@ -60,66 +63,67 @@ function calculateProjection(gameOdds, game) {
  * @returns {Boolean} true if bet365 object is present OR false
  */
 function hasBettingOdds(gameOddsData) {
-    if (gameOddsData.bet365) {
-        return true;
-    }
+  if (gameOddsData.bet365) {
+    return true;
+  }
 
-    return false;
+  return false;
 }
 
 /**
  * This function checks to see that final game results are present in the game score data
  * These fields are not present if the game is in progress or hasnt started so we need to
  * check before trying to access
+ * @param sport
  * @param {Object} gameScoreData - Object with the game's score information
  * @returns {Boolean} true if Win or Loss information is present OR false
  */
 function hasWinner(sport, gameScoreData) {
-    if (sport === constants.MLB) {
-        if (gameScoreData.homeResult && gameScoreData.awayResult) {
-            return true;
-        }
-    } else if (sport === constants.NBA) {
-        if (gameScoreData.gameClock.includes("Final")) {
-            return true;
-        }
+  if (sport === constants.MLB) {
+    if (gameScoreData.homeResult && gameScoreData.awayResult) {
+      return true;
     }
+  } else if (sport === constants.NBA) {
+    if (gameScoreData.gameClock.includes("Final")) {
+      return true;
+    }
+  }
 
-
-    return false;
+  return false;
 }
 
 /**
  * This function calls hasWinner to confirm that homeResult and awayResult exist before using
  * those values to determine who won the current game. If those fields don't exist "No Winner Yet"
+ * @param sport
  * @param {Object} gameScoreData - Object with the game's score information
  * @returns {String} calculated winning teams abbreviation or "No Winner Yet" if still live
  */
 function calculateWinner(sport, gameScoreData) {
-    if (hasWinner(sport, gameScoreData)) {
-        if (sport === constants.MLB) {
-            const {home, away, homeResult, awayResult} = gameScoreData;
+  if (hasWinner(sport, gameScoreData)) {
+    if (sport === constants.MLB) {
+      const { home, away, homeResult, awayResult } = gameScoreData;
 
-            if (homeResult.includes("W")) {
-                return home;
-            } else if (awayResult.includes("W")) {
-                return away;
-            }
-        } else if (sport === constants.NBA) {
-            console.log(gameScoreData);
-            const {home, away} = gameScoreData;
-            const homePoints = parseInt(gameScoreData.homePts);
-            const awayPoints = parseInt(gameScoreData.awayPts);
+      if (homeResult.includes("W")) {
+        return home;
+      } else if (awayResult.includes("W")) {
+        return away;
+      }
+    } else if (sport === constants.NBA) {
+      console.log(gameScoreData);
+      const { home, away } = gameScoreData;
+      const homePoints = parseInt(gameScoreData.homePts);
+      const awayPoints = parseInt(gameScoreData.awayPts);
 
-            if (homePoints > awayPoints) {
-                return home;
-            } else if (awayPoints > homePoints) {
-                return away;
-            }
-        }
+      if (homePoints > awayPoints) {
+        return home;
+      } else if (awayPoints > homePoints) {
+        return away;
+      }
     }
+  }
 
-    return "No Winner Yet";
+  return "No Winner Yet";
 }
 
 /**
@@ -130,15 +134,30 @@ function calculateWinner(sport, gameScoreData) {
  * @returns {Object} Object with game score, game status, and the calculated winner
  */
 function getScore(sport, gameScoreObj) {
-    const gameScoreData = gameScoreObj.body[Object.keys(gameScoreObj.body)[0]];
+  const gameScoreData = gameScoreObj.body[Object.keys(gameScoreObj.body)[0]];
 
-    return {
-        awayTeamScore: sport === constants.MLB ? gameScoreData.lineScore.away.R : sport ===  constants.NBA ? gameScoreData.awayPts : "0",
-        homeTeamScore: sport === constants.MLB ? gameScoreData.lineScore.home.R : sport ===  constants.NBA ? gameScoreData.homePts : "0",
-        gameTime: gameScoreData.gameTime,
-        status: sport === constants.MLB ? gameScoreData.currentInning : sport ===  constants.NBA ? gameScoreData.gameClock : "",
-        winner: calculateWinner(sport, gameScoreData)
-    }
+  return {
+    awayTeamScore:
+      sport === constants.MLB
+        ? gameScoreData.lineScore.away.R
+        : sport === constants.NBA
+          ? gameScoreData.awayPts
+          : "0",
+    homeTeamScore:
+      sport === constants.MLB
+        ? gameScoreData.lineScore.home.R
+        : sport === constants.NBA
+          ? gameScoreData.homePts
+          : "0",
+    gameTime: gameScoreData.gameTime,
+    status:
+      sport === constants.MLB
+        ? gameScoreData.currentInning
+        : sport === constants.NBA
+          ? gameScoreData.gameClock
+          : "",
+    winner: calculateWinner(sport, gameScoreData)
+  };
 }
 
 /**
@@ -147,35 +166,30 @@ function getScore(sport, gameScoreObj) {
  * @returns {Array} List of game Objects for the chosen date
  */
 export async function createGameRows(date, selectedSport) {
-    // if (selectedSport === constants.MLB) {
-    //     return mlbData;
-    // } else if (selectedSport === constants.NBA) {
-    //     return nbaData;
-    // }
-    let rows = [];
-    let gamesForDate = await getGameData(selectedSport, {gameDate: date.format("YYYYMMDD")}, CONFIG.endpoint[selectedSport].schedule);
+  let rows = [];
+  let gamesForDate = await getGameData(selectedSport, { gameDate: date.format("YYYYMMDD") }, CONFIG.endpoint[selectedSport].schedule);
 
-    if (typeof gamesForDate.body[0] !== "string") {
-        await Promise.all(gamesForDate.body.map(async (game) => {
-            const {gameID, away, home} = game;
-            const score = getScore(selectedSport, await getGameData(selectedSport, {gameID}, CONFIG.endpoint[selectedSport].scores));
-            const {awayTeamScore, homeTeamScore, status, winner, gameTime} = score;
+  if (typeof gamesForDate.body[0] !== "string") {
+    await Promise.all(gamesForDate.body.map(async (game) => {
+      const { gameID, away, home } = game;
+      const score = getScore(selectedSport, await getGameData(selectedSport, { gameID }, CONFIG.endpoint[selectedSport].scores));
+      const { awayTeamScore, homeTeamScore, status, winner, gameTime } = score;
 
-            const gameData = {
-                away,
-                awayTeamScore,
-                home,
-                homeTeamScore,
-                gameTime,
-                //projWinner: calculateProjection(await getGameData({gameID}, CONFIG.endpoint.odds), game),
-                status,
-                winner
+      const gameData = {
+        away,
+        awayTeamScore,
+        home,
+        homeTeamScore,
+        gameTime,
+        //projWinner: calculateProjection(await getGameData({gameID}, CONFIG.endpoint.odds), game),
+        status,
+        winner
 
-            }
+      };
 
-            rows.push(gameData);
-        }))
-    }
+      rows.push(gameData);
+    }));
+  }
 
-    return rows;
+  return rows;
 }
